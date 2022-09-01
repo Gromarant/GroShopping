e09ca0720b526556f2775080988f3b41dd5d384e
Mariangelica Rodriguez <gromarant@gmail.com>
2022-09-01 20:14:12 +0200

    add App component into components folder

---
+++ groshopping/src/components/App.jsx
@@ -0,0 +1,13 @@
+// import { Link, Route } from 'react-router-dom';
+import '../styles/App.css';
+import NavBar from './NavBar';
+
+function App() {
+  return (
+    <>
+      <NavBar /> 
+    </>
+  );
+}
+
+export default App;
