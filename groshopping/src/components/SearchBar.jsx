1dffa097ee21f13f7275d0920a6bd40676650890
Mariangelica Rodriguez <gromarant@gmail.com>
2022-09-02 16:00:18 +0200

    Add icons to the SearchBar component

--- groshopping/src/components/SearchBar.jsx
+++ groshopping/src/components/SearchBar.jsx
@@ -1,13 +1,28 @@
+import MicRoundedIcon from '@mui/icons-material/MicRounded';
+import SearchRoundedIcon from '@mui/icons-material/SearchRounded';
+import FilterListRoundedIcon from '@mui/icons-material/FilterListRounded';
+
 export const SearchBar = ( { placeholder, data } ) => {
     return (
         <>
             <form className='searchBar' role="search">
-                <input className='SearchBar__input' type="text" placeholder={placeholder} area-label="Search through site"></input>
-                <button className='SearchBar__button' type='submit'>Icon loupe</button>
-                <span className='SearchBar__filter'>Icon filter</span>
+                <input className='SearchBar__input' type="text" placeholder={ placeholder } area-label="Search through site"></input>
+                <span className='SearchBar__talkIcon'>
+                    <MicRoundedIcon />
+                </span>
+                <button className='SearchBar__searchIcon' type='submit'>
+                    <SearchRoundedIcon />
+                </button>
+                <span className='SearchBar__filterIcon'>
+                    <FilterListRoundedIcon />
+                </span>
             </form>
             <section className='data'>
-                <div className='data__results'></div>
+                <div className='data__results'>
+                    {data.map((value, key) => {
+                        return <div> {} </div>
+                    })}
+                </div>
             </section>
         </>
     );
