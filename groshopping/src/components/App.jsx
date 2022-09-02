2f8451b0e56d78e9ef70cbef5d89cab5edf15c4b
Mariangelica Rodriguez <gromarant@gmail.com>
2022-09-02 23:33:37 +0200

    fix path to SearchBar

--- groshopping/src/components/App.jsx
+++ groshopping/src/components/App.jsx
@@ -1,6 +1,6 @@
 import '../styles/App.css';
 import NavBar from '../components/NavBar';
-import { SearchBar } from './SearchBar';
+import { SearchBar } from '../components/SearchBar';
 
 
 function App() {
