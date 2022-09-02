ee99f686d6bb2c3cdef247f83448c444c407f4ad
Mariangelica Rodriguez <gromarant@gmail.com>
2022-09-02 13:41:10 +0200

    Make SearchBar component

--- groshopping/src/components/App.jsx
+++ groshopping/src/components/App.jsx
@@ -1,10 +1,13 @@
 import '../styles/App.css';
 import NavBar from './NavBar.jsx';
+import { SearchBar } from './SearchBar';
+
 
 function App() {
   return (
     <>
       <NavBar /> 
+      <SearchBar placeholder={"Search what you want"} data={"API data"}/>
     </>
   );
 }
--- groshopping/src/components/SearchBar.jsx
+++ groshopping/src/components/SearchBar.jsx
@@ -1,6 +1,14 @@
-export const SearchBar = () => {
+export const SearchBar = ( { placeholder, data } ) => {
     return (
         <>
+            <form className='searchBar' role="search">
+                <input className='SearchBar__input' type="text" placeholder={placeholder} area-label="Search through site"></input>
+                <button className='SearchBar__button' type='submit'>Icon loupe</button>
+                <span className='SearchBar__filter'>Icon filter</span>
+            </form>
+            <section className='data'>
+                <div className='data__results'></div>
+            </section>
         </>
     );
 }
